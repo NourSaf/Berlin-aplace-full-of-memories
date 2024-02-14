cff6901d7ac59e6e6a651dc7ce9c31d5685f3afd
Nour Al Safadi <nour.safadi7@icloud.com>
2024-02-14 16:53:58 +0100

    update

--- functions.js
+++ functions.js
@@ -5,11 +5,10 @@ var categories = {
     isVisible: true,
     locations: [
         {
-          name: '2016 - The first room in Berlin',
+          name: '2016 - MY FIRST WG',
           label: '🏠 Home',
           address: 'I was 19 years old when I first move to Berlin',
           location: [52.46980515548625, 13.426669208254209],
-          // photo: './Photos/home.jpg',
           link:"https://www.google.com/maps/place/Warthestra%C3%9Fe+64,+12051+Berlin/@52.4698348,13.4241148,17z/data=!3m1!4b1!4m6!3m5!1s0x47a84f90b26bae33:0x42a76427bdfab9e9!8m2!3d52.4698348!4d13.4266897!16s%2Fg%2F11bw3frbnf?entry=ttu",
           music: './music/die welt geht runter.mp3'
         },
@@ -19,7 +18,6 @@ var categories = {
           label: '🏠 Home',
           address: 'It was one of the most unique experiences I have ever had.',
           location: [52.47869311696065, 13.423709420621154],
-          // photo: './Photos/home.jpg',
           link:"https://www.google.com/maps/place/Weisestra%C3%9Fe+59,+12049+Berlin/@52.4787029,13.4235311,20z/data=!4m6!3m5!1s0x47a84fbe868a3cc7:0x39139f0b9f0ed03f!8m2!3d52.47868!4d13.4237007!16s%2Fg%2F11csjtm8hj?entry=ttu",
           music: './music/Lights.mp3'
         },           
@@ -29,7 +27,6 @@ var categories = {
           label: '🏠 Home',
           address: 'In was then, when I met Benjamen and broke up with Caroline. It was the phase of partiying alot',
           location: [52.511088006963945, 13.415326989528703],
-          // photo: './Photos/home.jpg',
           link:"https://www.google.com/maps/place/K%C3%B6penicker+Str.+99,+10179+Berlin/@52.5110472,13.4146967,19z/data=!3m1!4b1!4m6!3m5!1s0x47a84e251132cb31:0xf71361e2fab1f74d!8m2!3d52.5110472!4d13.4153404!16s%2Fg%2F11c1xqsmlk?entry=ttu",
           music: './music/Cake.mp3'
         },
@@ -39,7 +36,6 @@ var categories = {
           label: '🏠 Home',
           address: 'Only one good thing was that I met Ronja',
           location: [52.50443974227214, 13.428637823104616],
-          // photo: './Photos/home.jpg',
           link:"https://www.google.com/maps/place/Wrangelstra%C3%9Fe+130,+10997+Berlin/@52.50427,13.4260951,17z/data=!3m1!4b1!4m6!3m5!1s0x47a84e36256af43f:0xa896b98f55a69e02!8m2!3d52.50427!4d13.42867!16s%2Fg%2F11c5jdjmr7?entry=ttu",
           music: './music/redbone.mp3'
         },
@@ -49,7 +45,6 @@ var categories = {
           label: '🏠 Home',
           address: 'Here I met Qusai, and we became best friends; I learned how to trust.',
           location: [52.54071604760491, 13.41260941677279],
-          // photo: './Photos/home.jpg',
           link:"https://www.google.com/maps/place/Sch%C3%B6nhauser+Allee+45,+10435+Berlin/@52.5404634,13.4120859,19.91z/data=!4m6!3m5!1s0x47a851ff0a99f59d:0xe29b3da88523bd14!8m2!3d52.5406949!4d13.4126044!16s%2Fg%2F11c5ps14kq?entry=ttu",
           music: './music/ijustcannot.mp3'
         },
@@ -59,7 +54,6 @@ var categories = {
             label: '🏠 Home',
             address: 'I lived 3 years with Naoko and Sarah. Here I learned what is it like to change yourself. I learned a bit about caring',
             location: [52.491629492129, 13.392841578747062],
-            // photo: './Photos/home.jpg',
             link:"https://www.google.com/maps/place/Gneisenaustra%C3%9Fe+17,+10961+Berlin/@52.4915317,13.3903525,17z/data=!3m1!4b1!4m6!3m5!1s0x47a84fd7dd89510d:0x1b257c405f64ca80!8m2!3d52.4915317!4d13.3929274!16s%2Fg%2F11b8v57b5x?entry=ttu",
             music: './music/thank god.mp3'
         },
@@ -69,7 +63,6 @@ var categories = {
             label: '🏠 Home',
             address: 'After a long time living in shared apartments, I decided to live alone, and here we go',
             location: [52.51495800109478, 13.433271772041314],
-            // photo: './Photos/home.jpg',
             link:"https://www.google.com/maps/place/Singerstra%C3%9Fe+33,+10243+Berlin/@52.5148014,13.4284545,17z/data=!4m15!1m8!3m7!1s0x47a84e3f723ea133:0xda49e3ecc29e165a!2sSingerstra%C3%9Fe+33,+10243+Berlin!3b1!8m2!3d52.5148015!4d13.4333254!16s%2Fg%2F11c2h490jy!3m5!1s0x47a84e3f723ea133:0xda49e3ecc29e165a!8m2!3d52.5148015!4d13.4333254!16s%2Fg%2F11c2h490jy?entry=ttu",
             music: './music/kanet.mp3'
         },
@@ -86,7 +79,6 @@ var categories = {
           label: '🍻  The best juice',
           address: "In summer you can not miss it ",
           location: [52.54177915548525, 13.406139859437936],
-          // photo: './Photos/home.jpg',
           link:"https://www.google.com/maps/place/Juicery+Berlin/@52.5323852,13.4049924,13.96z/data=!4m6!3m5!1s0x47a851f8fb9d61ed:0xa9f18b326db1b169!8m2!3d52.5402193!4d13.405828!16s%2Fg%2F11b6371t6s?entry=ttu",
           music: './music/kanet.mp3'
         },
@@ -96,7 +88,6 @@ var categories = {
           label: '🍻  Punk Rock',
           address: "I used to go here with Lisa on thursdays for a Rock concert",
           location: [52.53233538203483, 13.411964610534843],
-          // photo: './Photos/home.jpg',
           link:"https://www.google.com/maps/place/8MM+Bar/@52.5323895,13.4049692,13.96z/data=!3m1!5s0x47a851e2abb10bef:0x4ac9049c365be1e6!4m6!3m5!1s0x47a851e2aac77ca1:0xa60547d856e96adf!8m2!3d52.531218!4d13.411524!16s%2Fg%2F1tx8s768?entry=ttu",
           music: './music/jumpsuit.mp3'
         },
@@ -106,7 +97,6 @@ var categories = {
           label: '🍻  Summer Chillout',
           address: "Huuuuf, a lot of things happened here. Once with Siwar and Steffi. With Adham, Qusai and Daniel. With Ronja and Lisa. With the other Lisa in Shook Concert.",
           location: [52.51195781032414, 13.42738829249855],
-          // photo: './Photos/home.jpg',
           link:"https://www.google.com/maps/place/Holzmarkt+25/@52.510468,13.4078305,14.87z/data=!4m6!3m5!1s0x47a84e3963e235b5:0xb044835f7d3a94ec!8m2!3d52.5113137!4d13.4268387!16s%2Fg%2F11k9h3h915?entry=ttu",
           music: './music/lala.mp3'
         },
@@ -116,7 +106,6 @@ var categories = {
           label: '🍻  Standup Comedy show',
           address: "The night I went to see Salam in the standup comedy show",
           location: [52.49242156227893, 13.438387368506842],
-          // photo: './Photos/home.jpg',
           link:"https://www.google.com/maps/place/Biergarten+Jockel/@52.4917993,13.4250856,15.12z/data=!4m6!3m5!1s0x47a84fad83ee12cf:0x32222854a7e82f96!8m2!3d52.4919277!4d13.4385616!16s%2Fg%2F1v16pb3n?entry=ttu",
           music: './music/lala.mp3'
         },
@@ -126,7 +115,6 @@ var categories = {
           label: '🍻  It feels like summer',
           address: "The place to be in summer",
           location: [52.47562757422759, 13.400932693022977],
-          // photo: './Photos/home.jpg',
           link:"https://www.google.com/maps/place/Tempelhofer+Feld/@52.482341,13.4086326,14.18z/data=!4m6!3m5!1s0x47a84fe8f7d899eb:0x88898e99acbb718b!8m2!3d52.4748569!4d13.4005926!16s%2Fg%2F1213_1c4?entry=ttu",
           music: './music/lala.mp3'
         },
@@ -136,7 +124,6 @@ var categories = {
           label: '🍻 Remote working and hang out with Naoko',
           address: "Here was the first time I went to ",
           location: [52.49077852861389, 13.394500568020419],
-          // photo: './Photos/home.jpg',
           link:"https://www.google.com/maps/place/Tempelhofer+Feld/@52.482341,13.4086326,14.18z/data=!4m6!3m5!1s0x47a84fe8f7d899eb:0x88898e99acbb718b!8m2!3d52.4748569!4d13.4005926!16s%2Fg%2F1213_1c4?entry=ttu",
           music: './music/lala.mp3'
         },
@@ -154,7 +141,6 @@ var categories = {
       label: '👨🏾‍💻 Work',
       address: 'My first job as a designer in a relative big agency. The people there were amazing. I will not foget the team',
       location: [52.491154631164704, 13.41858690508184],
-      photo: './Photos/work.png',
       link:"https://www.google.com/maps/place/:+HENKELHIEDL+(Kreativagentur%2FDigitalagentur)/@52.4915317,13.3929274,17z/data=!4m6!3m5!1s0x47a84fc90bcb9829:0xb3d5dddcab2f1890!8m2!3d52.4910175!4d13.418662!16s%2Fg%2F1td64bf1?entry=ttu",
       music: './music/shab.mp3'
 
@@ -165,7 +151,6 @@ var categories = {
       label: '👨🏾‍💻 Work',
       address: 'The first job at the university',
       location: [52.51717650420129, 13.322650234934772],
-      // photo: './Photos/work.png',
       link:"https://www.google.com/maps/place/Designtransfer/@52.5170721,13.3200646,17z/data=!3m1!4b1!4m6!3m5!1s0x47a8511bb2853333:0x69af5b7d3013aaea!8m2!3d52.5170721!4d13.3226395!16s%2Fg%2F1pxwr3b4f?entry=ttu",
       music: './music/ilka.mp3'
 
@@ -176,7 +161,6 @@ var categories = {
       label: '👨🏾‍💻 Work',
       address: 'What a time there!',
       location: [52.510125566558585, 13.29945030092248],
-      // photo: './Photos/work.png',
       link:"https://www.google.com/maps/place/Bosch+Service+Solutions/@52.5099232,13.2967681,17z/data=!3m1!4b1!4m6!3m5!1s0x47a850d8c3f49755:0xe32ecf22de9a8ec9!8m2!3d52.5099232!4d13.299343!16s%2Fg%2F11ggb93pt0?entry=ttu",
       music: './music/track.mp3'
 
@@ -194,7 +178,6 @@ var categories = {
         label: '🏫 Study',
         address: 'Here I studied visual communication at the class of Information design. I learned a lot.',
         location: [52.49099156128556, 13.358810151068894],
-        photo: './Photos/uni.jpeg',
         link:"https://www.google.com/maps/place/Universit%C3%A4t+der+K%C3%BCnste,+Medienhaus/@52.4908283,13.3562567,17z/data=!3m1!4b1!4m6!3m5!1s0x47a8504712a30e33:0x42e91f98e8b32325!8m2!3d52.4908283!4d13.3588316!16s%2Fg%2F1tp_5dyt?entry=ttu",
         music: './music/bachelor.mp3'
 
@@ -231,10 +214,8 @@ Object.keys(categories).forEach(function (categoryName) {
 
     marker.addTo(map);
     category.markers.push(marker);
-
   });
 });
-
 
 var circleStyle = {
   color: 'none', 
@@ -346,6 +327,5 @@ function closeOverlay() {
       window.currentCircle = null;
   }
   document.getElementById('overlay').style.top = '-100%'; // Hide the overlay
-  // Show music controls
-  document.getElementById('musicControls').style.display = 'block';
-}
+ 
+} 
